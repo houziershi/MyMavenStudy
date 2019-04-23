4916e55628441447f791d4dba7cc5baaaa331f56
houguokun@lazyaudio.com <Hou1234>
2019-04-23 09:57:36 +0800

    first commit

---
+++ mymaven/src/main/java/com/example/yyting/mymaven/MyUtils.java
@@ -0,0 +1,11 @@
+package com.example.yyting.mymaven;
+
+/**
+ * Discription:
+ * Created by guokun on 2019/4/23.
+ */
+public class MyUtils {
+    public static String getMyMaven() {
+        return "get string maven Hello World!!!!!!!!!!!";
+    }
+}
